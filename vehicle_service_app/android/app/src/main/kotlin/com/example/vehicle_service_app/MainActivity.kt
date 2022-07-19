469a50652702b695705a00691cd851ecdb40916b
Mohammed Dayyan <mohammeddayan29@gmail.com>
2022-07-19 10:51:36 +0000

    project initiation

---
+++ vehicle_service_app/android/app/src/main/kotlin/com/example/vehicle_service_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vehicle_service_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ vehicle_service_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:vehicle_service_app/ui/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Vehicle Service',
+      home: Homepage(),
+    );
+  }
+}
