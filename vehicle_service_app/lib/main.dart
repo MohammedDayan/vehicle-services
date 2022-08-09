c6138a0b21de206501f2540e96277606fc35bcac
Mohammed Dayyan <mohammeddayan29@gmail.com>
2022-08-09 16:13:52 +0000

    homepage

--- vehicle_service_app/lib/main.dart
+++ vehicle_service_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vehicle_service_app/ui/homepage.dart';
+import 'package:vehicle_service_app/app_ui/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Colors.white,
       title: 'Vehicle Service',
       home: Homepage(),
     );
